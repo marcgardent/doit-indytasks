05065e1a2d58c8724cd5b8cb4696fa7e49bf86b4
marcgardent <marcgardent@yahoo.fr>
2022-02-14 12:43:58 +0100

    release version 0.0.7

--- dodo.py
+++ dodo.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 import re
 from doit.tools import exceptions
 import os
@@ -7,14 +8,15 @@ RELEASE = 'release'
 CFG_FILE = "setup.cfg"
 README_FILE = "README.md"
 
-def replace_in_file(file, regex_source, regex_dest):     
+def replace_in_file(file, regex_source, regex_dest, dryrun=False):     
     import re           
     print(f"replace in '{file}' /{regex_source}/ by /{regex_dest}/ ...")
     content_new = ""
     with open (file, 'r', encoding='utf-8' ) as f:
         content_new = re.sub(regex_source, regex_dest, f.read(), flags = re.M)
-    with open (file, 'w', encoding='utf-8' ) as f:
-        f.write(content_new)
+    if not dryrun:
+        with open (file, 'w', encoding='utf-8' ) as f:
+            f.write(content_new)
 
 def task_ready():
     """folder is ready for a new task?"""
@@ -45,27 +47,30 @@ def task_release():
             os.system("git tag --list")
             return exceptions.TaskFailed("change the version")
 
-    def update_setup(version):
-        replace_in_file(CFG_FILE, r'^version = \d+\.\d+\.\d+$',f"version = {version}")
+    def update_setup(version, dryrun):
+        replace_in_file(CFG_FILE, r'^version = \d+\.\d+\.\d+$',f"version = {version}", dryrun)
 
-    def update_readme(version):
-        replace_in_file(README_FILE, r'@v\d+\.\d+\.\d+',f"@v{version}")
+    def update_readme(version, dryrun):
+        replace_in_file(README_FILE, r'@v\d+\.\d+\.\d+',f"@v{version}", dryrun)
 
-    def commit(version):
+    def commit(version, dryrun):
         cmd = f"git commit -a -m \"release version {version}\""
         print (cmd)
-        return os.system(cmd) != 0
+        return True if dryrun else os.system(cmd) == 0
 
-    def tag(version):
+    def tag(version, dryrun):
         cmd = f"git tag v{version} --sign -m \"release version {version}\""
         print (cmd)
-        return os.system(cmd) != 0
-
+        return True if dryrun else os.system(cmd) == 0
+            
     return {
         'basename': RELEASE,
-        'params': [{'name': 'version', 'short': 'v', 'default': "0.0.0"}],
+        'params': [
+            {'name': 'version', 'short': 'v', 'default': "0.0.0"},
+            {'name': 'dryrun', 'short': 'd', 'type' : 'bool' , 'default': False }
+            ],
         'task_dep': [],
         'targets': [CFG_FILE, README_FILE],
-        'actions': [(check_format,), (check_tag,), (update_setup,),  (update_readme,), (commit,), (tag,), "git push --all --porcelain"],
+        'actions': [(update_setup,),  (update_readme,), (commit,), (tag,), "git push --all --porcelain"],
         'verbosity': 2   
     }
